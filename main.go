b2f797945d28eadc86f44c58c78b3109f933dc93
Kambar-ZH <zhamauov02@mail.ru>
2021-10-23 19:34:24 +0600

    upper and lower functions v2.0.0

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/Kambar-ZH/Golang_Onelab/utils"
 )
 
 func main() {
-	fmt.Println(strings.Upper("Kambar"))
-	fmt.Println(strings.Lower("Kambar"))
+	fmt.Println(utils.Upper("Kambar"))
+	fmt.Println(utils.Lower("Kambar"))
 }
---
+++ utils/square.go
@@ -0,0 +1,5 @@
+package utils
+
+func Square(x int) int {
+	return x * x
+}
